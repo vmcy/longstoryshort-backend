66528ebe2f3a24ee442156f626145954c13ebf29
vince-mah <vince.mah@marinabaysands.com>
2025-02-16 23:03:39 +0800

    added unit testing for expandUrl function

--- longstoryshort/tests/unit/test-handler.mjs
+++ longstoryshort/tests/unit/test-handler.mjs
@@ -17,15 +17,15 @@ describe('Shorten URL', function () {
         expect(result.messageCode).to.equal(0);
         expect(result.message).to.be.equal("URL successfully shortened");
 
-        let data = JSON.parse(result.data);
+        let data = result.data;
         expect(data).to.be.an('object');
         expect(data.originalUrl).to.be.equal(event.body.originalUrl);
         expect(data.shortUrl).to.be.an('string');
-        expect(data.shortUrl).to.have.lengthOf(1);
+        expect(data.shortUrl).to.have.length.above(1);
     });
 
     // Should throw error for missing original URL
-    it('verifies empty original url response', async () => {
+    it('verifies missing original url response', async () => {
         const response = await shortenUrl(event, context)
         expect(response).to.be.an('object');
         expect(response.statusCode).to.equal(400);
@@ -38,7 +38,7 @@ describe('Shorten URL', function () {
     });
 
     // Should throw error for invalid original URL
-    it('verifies invalid url response', async () => {
+    it('verifies invalid original url response', async () => {
         const response = await shortenUrl(event, context)
         expect(response).to.be.an('object');
         expect(response.statusCode).to.equal(422);
@@ -53,8 +53,61 @@ describe('Shorten URL', function () {
 
 describe('Expand URL', function () {
     // Should expand a shortened URL correctly
+    it('verifies successful response', async () => {
+        const response = await expandUrl(event, context)
+        expect(response).to.be.an('object');
+        expect(response.statusCode).to.equal(200);
+        expect(response.body).to.be.an('string');
+
+        let result = JSON.parse(response.body);
+        expect(result).to.be.an('object');
+        expect(result.messageCode).to.equal(0);
+        expect(result.message).to.be.equal("URL successfully expanded");
+
+        let data = JSON.parse(result.data);
+        expect(data).to.be.an('object');
+        expect(data.shortUrl).to.be.equal(event.body.shortUrl);
+        expect(data.expandUrl).to.be.an('string');
+        expect(data.expandUrl).to.have.lengthOf(1);
+        expect(data.expandUrl).to.be.equal("https://forum.lowyat.net");
+    });
 
     // Should throw error for missing short URL
+    it('verifies missing short url response', async () => {
+        const response = await expandUrl(event, context)
+        expect(response).to.be.an('object');
+        expect(response.statusCode).to.equal(400);
+        expect(response.body).to.be.an('string');
+
+        let result = JSON.parse(response.body);
+        expect(result).to.be.an('object');
+        expect(result.messageCode).to.equal(1);
+        expect(result.message).to.be.equal("missing required field: url");
+    });
+
+    // Should throw error for invalid short URL
+    it('verifies invalid short url response', async () => {
+        const response = await expandUrl(event, context)
+        expect(response).to.be.an('object');
+        expect(response.statusCode).to.equal(422);
+        expect(response.body).to.be.an('string');
+
+        let result = JSON.parse(response.body);
+        expect(result).to.be.an('object');
+        expect(result.messageCode).to.equal(2);
+        expect(result.message).to.be.equal("invalid url provided");
+    });
 
     // Should throw error for non-existent short URL
+    it('verifies non-existent short url response', async () => {
+        const response = await expandUrl(event, context)
+        expect(response).to.be.an('object');
+        expect(response.statusCode).to.equal(404);
+        expect(response.body).to.be.an('string');
+
+        let result = JSON.parse(response.body);
+        expect(result).to.be.an('object');
+        expect(result.messageCode).to.equal(3);
+        expect(result.message).to.be.equal("non-existent short url provided");
+    });
 });
