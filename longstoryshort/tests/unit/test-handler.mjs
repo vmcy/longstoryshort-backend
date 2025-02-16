ee886379c13abbc4dd6027bea7ab86521f07dc45
vince-mah <vince.mah@marinabaysands.com>
2025-02-16 15:37:10 +0800

    added unit testing for shortenUrl function

--- longstoryshort/tests/unit/test-handler.mjs
+++ longstoryshort/tests/unit/test-handler.mjs
@@ -4,29 +4,57 @@ import { shortenUrl, expandUrl } from '../../app.mjs';
 import { expect } from 'chai';
 var event, context;
 
-describe('Tests index', function () {
+describe('Shorten URL', function () {
+    // Should shorten valid URL
     it('verifies successful response', async () => {
-        const result = await shortenUrl(event, context)
-
-        expect(result).to.be.an('object');
-        expect(result.statusCode).to.equal(200);
-        expect(result.body).to.be.an('string');
-
-        let response = JSON.parse(result.body);
-
+        const response = await shortenUrl(event, context)
         expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("Shortern URL Success");
+        expect(response.statusCode).to.equal(200);
+        expect(response.body).to.be.an('string');
+
+        let result = JSON.parse(response.body);
+        expect(result).to.be.an('object');
+        expect(result.messageCode).to.equal(0);
+        expect(result.message).to.be.equal("URL successfully shortened");
+
+        let data = JSON.parse(result.data);
+        expect(data).to.be.an('object');
+        expect(data.originalUrl).to.be.equal(event.body.originalUrl);
+        expect(data.shortUrl).to.be.an('string');
+        expect(data.shortUrl).to.have.lengthOf(1);
     });
 
-    // Should shorten valid URL
+    // Should throw error for missing original URL
+    it('verifies empty original url response', async () => {
+        const response = await shortenUrl(event, context)
+        expect(response).to.be.an('object');
+        expect(response.statusCode).to.equal(400);
+        expect(response.body).to.be.an('string');
 
+        let result = JSON.parse(response.body);
+        expect(result).to.be.an('object');
+        expect(result.messageCode).to.equal(1);
+        expect(result.message).to.be.equal("missing required field: url");
+    });
+
+    // Should throw error for invalid original URL
+    it('verifies invalid url response', async () => {
+        const response = await shortenUrl(event, context)
+        expect(response).to.be.an('object');
+        expect(response.statusCode).to.equal(422);
+        expect(response.body).to.be.an('string');
+
+        let result = JSON.parse(response.body);
+        expect(result).to.be.an('object');
+        expect(result.messageCode).to.equal(2);
+        expect(result.message).to.be.equal("invalid url provided");
+    });
+});
+
+describe('Expand URL', function () {
     // Should expand a shortened URL correctly
 
-    // Should throw error for invalid URL
+    // Should throw error for missing short URL
 
     // Should throw error for non-existent short URL
-
-    // Should throw error for empty short URL
-
-    // Show throw error for empty expand URL
 });
