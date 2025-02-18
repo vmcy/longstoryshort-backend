9db925d432985c56162069a00156fc52f8a246bd
vmcy <mahcyen@gmail.com>
2025-02-18 17:39:25 +0800

    updated function shortenUrl with DynamoDB insertion feature upon creation of short url

--- longstoryshort/app.mjs
+++ longstoryshort/app.mjs
@@ -1,5 +1,11 @@
 import { isValidUrl, hasRequiredProperty, generateUniqueID } from './utils.mjs';
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+
 import config from './config.mjs';
+
+const ddbClient = new DynamoDBClient({});
+const ddbDocClient = DynamoDBDocumentClient.from(ddbClient);
 
 export const shortenUrl = async (event, context) => {
   let body;
@@ -33,19 +39,41 @@ export const shortenUrl = async (event, context) => {
     };
   }
 
-  const successResponse = {
-    statusCode: 200,
-    body: JSON.stringify({
-      messageCode: 0,
-      message: 'URL successfully shortened',
-      data: {
-        originalUrl: originalUrl,
-        shortUrl: `${config.DOMAIN}/${generateUniqueID()}`
-      }
-    })
-  };
+  const shortUrlId = generateUniqueID();
 
-  return successResponse;
+  const ddbPutCommand = new PutCommand({
+    TableName: "UrlShortenerTable",
+    Item: {
+      ShortId: shortUrlId,
+      OriginalUrl: originalUrl,
+      CreatedTimestamp: Date.now(),
+      ExpiryTimestamp: Date.now() + config.DEFAULT_SHORT_URL_EXPIRY_DURATION_DAY * 24 * 60 * 60 * 1000
+    }
+  });
+
+  try {
+    await ddbDocClient.send(ddbPutCommand);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        messageCode: 0,
+        message: 'URL successfully shortened',
+        data: {
+          originalUrl: originalUrl,
+          shortUrl: `${config.DOMAIN}/${generateUniqueID()}`
+        }
+      })
+    };
+  } catch (error) {
+    console.error('Error inserting item: ', error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        messageCode: 99,
+        message: `unexpected error: ${error.stack}`,
+      })
+    };
+  }
 };
 
 export const expandUrl = async (event, context) => {
